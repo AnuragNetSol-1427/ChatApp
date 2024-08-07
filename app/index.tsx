0fe0bddfd83c68d6c7a065777a8b93580b8e20dd
Anurag Sharma <anurag.sharma@netsolutions.com>
2024-08-07 18:47:50 +0530

    feat: setup tailwind with nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { View, Text, SafeAreaView, Platform } from 'react-native'
+import React from 'react'
 
-export default function Index() {
+const Home = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <SafeAreaView style={Platform.OS == 'android' && {paddingTop: 40}} className='bg-red-500 '>
+      <Text className='text-3xl text-center'>Home</Text>
+    </SafeAreaView>
+  )
 }
+
+export default Home;
